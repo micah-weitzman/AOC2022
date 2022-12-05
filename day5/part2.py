0e370625d92b72adf5fbb03fae673e573c460d74
Micah Weitzman <mikachoow21@gmail.com>
2022-12-04 22:28:19 -0800

    minor cosmetic stuff

--- day5/part2.py
+++ day5/part2.py
@@ -1,15 +1,17 @@
 from collections import deque
 import re
+"""
+                  [B]     [L]     [S]
+          [Q] [J] [C]     [W]     [F]
+      [F] [T] [B] [D]     [P]     [P]
+      [S] [J] [Z] [T]     [B] [C] [H]
+      [L] [H] [H] [Z] [G] [Z] [G] [R]
+  [R] [H] [D] [R] [F] [C] [V] [Q] [T]
+  [C] [J] [M] [G] [P] [H] [N] [J] [D]
+  [H] [B] [R] [S] [R] [T] [S] [R] [L]
+   1   2   3   4   5   6   7   8   9 
+"""
 
-#                 [B]     [L]     [S]
-#         [Q] [J] [C]     [W]     [F]
-#     [F] [T] [B] [D]     [P]     [P]
-#     [S] [J] [Z] [T]     [B] [C] [H]
-#     [L] [H] [H] [Z] [G] [Z] [G] [R]
-# [R] [H] [D] [R] [F] [C] [V] [Q] [T]
-# [C] [J] [M] [G] [P] [H] [N] [J] [D]
-# [H] [B] [R] [S] [R] [T] [S] [R] [L]
-#  1   2   3   4   5   6   7   8   9 
 
 # b1 = deque(["R", "C", "H"][::-1])
 # b2 = deque(["F","S","L","H","J","B"][::-1])
